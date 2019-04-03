8ef7a09641e149ec2bbda5f15d9ab57c95d60b0b
transtone <zm3345@126.com>
2019-04-03 15:07:48 +0800

    change cr to lf

--- build/publish.sh
+++ build/publish.sh
@@ -1,1 +1,15 @@
-#!/usr/bin/env zshset -eecho "Enter release version: "read VERSIONread -p "Releasing $VERSION - are you sure? (y/n)" replyecho    # (optional) move to a new lineif [ $reply = "y" ]then  # build  yarn build:lib  npm version $VERSION --message "[release] $VERSION"  npm publish --access=publicfi
+#!/usr/bin/env zsh
+set -e
+echo "Enter release version: "
+read VERSION
+
+read -p "Releasing $VERSION - are you sure? (y/n)" reply
+echo    # (optional) move to a new line
+if [ $reply = "y" ]
+then
+  # build
+  yarn build:lib
+  npm version $VERSION --message "[release] $VERSION"
+  npm publish --access=public
+
+fi
